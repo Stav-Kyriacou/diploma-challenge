8cf3fb84e2b373c64792ff81a5fca7954766d1cb
Stav-Kyriacou <101610510@student.swin.edu.au>
2022-06-27 14:00:22 +1000

    create order complete

--- website/src/app/components/main/main.component.ts
+++ website/src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer, MainService, Order, Product } from 'src/app/services/main.service';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  shipMode: string[] = ["Second Class", "Standard Class", "First Class", "Overnight Express"];
+  orders: Order[] = [];
+  customers: Customer[] = [];
+  products: Product[] = [];
+  createCustID: string = "";
+  createProdID: string = "";
+  createOrderDate: Date = new Date(2022, 6, 27);
+  createShipDate: Date = new Date(2022, 6, 27);
+  createQuantity: number = 1;
+  createShipMode: string = this.shipMode[0];
 
-  constructor() { }
+
+  constructor(private _mainService: MainService) { }
 
   ngOnInit(): void {
-  }
+    this.fetchData();
 
+  }
+  onChange(event: any) {
+
+  }
+  shippingChange(event: any) {
+    console.log(event);
+
+  }
+  test() {
+    console.log(this.createCustID);
+    console.log(this.createProdID);
+    console.log(this.createOrderDate.toJSON());
+    console.log(this.createShipDate.toJSON());
+    console.log(this.createQuantity);
+    console.log(this.createShipMode);
+  }
+  createOrder() {
+    this._mainService.createOrder(this.createCustID, this.createProdID, this.createQuantity, this.createOrderDate, this.createShipDate, this.createShipMode).subscribe(null, null, () => {
+      this.fetchData();
+    });
+  }
+  fetchData() {
+    this._mainService.getAllOrders().subscribe(data => this.orders = data);
+    this._mainService.getAllCustomers().subscribe(data => this.customers = data, null, () => {
+      this.createCustID = this.customers[0].custID;
+    });
+    this._mainService.getAllProducts().subscribe(data => this.products = data, null, () => {
+      this.createProdID = this.products[0].prodID;
+    });
+  }
 }
--- website/src/app/services/main.service.ts
+++ website/src/app/services/main.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Data } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,52 @@ export class MainService {
   readonly baseUrl: string = "https://localhost:5001";
   // readonly baseUrl: string = "https://101610510-challenge-api.azurewebsites.net";
 
-    constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
+  getAllOrders(): Observable<Order[]> {
+    return this._http.get<Order[]>(this.baseUrl + '/orders');
+  }
+  getAllCustomers(): Observable<Customer[]> {
+    return this._http.get<Customer[]>(this.baseUrl + '/customers');
+  }
+  getAllProducts(): Observable<Product[]> {
+    return this._http.get<Product[]>(this.baseUrl + '/products');
+  }
+  createOrder(custID: string, prodID: string, quantity: number, orderDate: Date, shipDate: Date, shipMode: string): Observable<number> {
+    const body = "";
+    const params = new HttpParams()
+      .append('custID', custID)
+      .append('prodID', prodID)
+      .append('quantity', quantity)
+      .append('orderDate', orderDate.toJSON())
+      .append('shipDate', shipDate.toJSON())
+      .append('shipMode', shipMode)
+
+    return this._http.post<number>(this.baseUrl + '/create-order', body, {'params': params});
+  }
+}
+export interface Order {
+  orderID: number;
+  orderDate: Date;
+  quantity: number;
+  shipDate: Date;
+  custID: string;
+  prodID: string;
+  shipMode: string;
+}
+export interface Customer {
+  custID: string;
+  fullName: string;
+  country: string;
+  city: string;
+  state: string;
+  postCode: number;
+  segID: number;
+  region: string;
+}
+export interface Product {
+  prodID: string;
+  description: string;
+  unitPrice: number;
+  catID: number
 }
