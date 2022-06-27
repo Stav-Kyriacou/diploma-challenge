91be0b802b63b9b55bfc8ddb99d54eda79fb9662
Stav-Kyriacou <101610510@student.swin.edu.au>
2022-06-27 16:23:31 +1000

    change api url to deployment url

--- website/src/app/services/main.service.ts
+++ website/src/app/services/main.service.ts
@@ -7,8 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class MainService {
-  readonly baseUrl: string = "https://localhost:5001";
-  // readonly baseUrl: string = "https://101610510-challenge-api.azurewebsites.net";
+  // readonly baseUrl: string = "https://localhost:5001";
+  readonly baseUrl: string = "https://101610510-challenge-api.azurewebsites.net";
 
   constructor(private _http: HttpClient, public datePipe: DatePipe) { }
 
